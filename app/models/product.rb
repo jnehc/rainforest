a01401ce041c8ca3f5c131d91564558d78ea29e4
jhc <coombsjh@gmail.com>
2015-02-10 00:11:23 -0500

    Belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,13 @@
 class Product < ActiveRecord::Base
-  validates :description, :name, presence: true
-  validates :price_in_cents, numericality: {only_integer: true}
- 
- def formatted_price
+def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
     sprintf("%.2f", price_in_dollars)
   end
+validates :description, :name, presence: true
+validates :price_in_cents, numericality: {only_integer: true}
 
-
+  has_many :reviews
+  has_many :users, through: :reviews
 end
-# add scopes here
+
+  # add scopes here
